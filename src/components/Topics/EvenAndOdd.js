9e1ad6b00865504751da5aa491bfb2c8917d7b22
Jon-Erik Nissen <jenissen@me.com>
2020-06-18 02:26:30 -0700

    found solutions for first two probs

--- src/components/Topics/EvenAndOdd.js
+++ src/components/Topics/EvenAndOdd.js
@@ -1,4 +1,13 @@
 export default class EvenAndOdd extends Component{
+    constructor(evenArray, oddArray, userInput = ''){
+        super();
+        this.state = {
+            evenArray: [],
+            oddArray: [],
+            userInput: ''
+        }
+    }
+
     render (){
         return (
             <div className="puzzleBox evenAndOddPB">
@@ -10,12 +19,15 @@ export default class EvenAndOdd extends Component{
             </div>
         )
     }
-    constructor (evenArray = [], oddArray = [], userInput = ''){
-        super();
-        this.state = {
-            evenArray: [],
-            oddArray: [],
-            userInput: ''
-        }
+
+    onChange(){
+        this.state.userInput = inpu
+    }
+    
+    assignEvensAndOdds(userInput){
+        let numsArr = eval(`[${userInput}]`);
+        this.state.evenArray = numsArr.filter(num=>!num%2);
+        this.state.oddArray = numsArr.filter(num=>num%2);
+        // are lines 28-29 equivalent to this.setState({evenArray: numsArr.filter(num=>!num%2), oddArray: numsArr.filter(num=>num%2)})?
     }
 }
--- src/components/Topics/FilterObject.js
+++ src/components/Topics/FilterObject.js
@@ -1,6 +1,58 @@
 export default class FilterObject extends Component{
-    render (){
-        return <p>FilterObject</p>
+    constructor(){
+        super();
+        this.state = {
+            dispensaries: [
+                {
+                    name: 'Holistic Center',
+                    location: 'North Phoenix',
+                    price: 'Budget',
+                    yearsOpen: 3
+                    
+                },
+                {
+                    name: 'Debbies',
+                    location: 'North-West Valley',
+                    price: 'High',
+                    yearsOpen: 5
+                },
+                {
+                    name: 'Med-Mens',
+                    location: 'Scottsdale',
+                    price: 'Very High',
+                    yearsOpen: 7
+                },
+                {
+                    name: 'Good Dispensary',
+                    location: 'Mesa',
+                    price: 'Low',
+                    yearsOpen: 4
+                }
+            ],
+            userInput: '',
+            filteredDispensaries: []
+        }
+        
+    }
+    handleChange(val){
+        this.setState({userInput: val});
+    }
+    filterDispensaries(prop){
+        let dispensaries = this.state.dispensaries;
+        let filteredDispensaries = [];
+        filteredDispensaries = dispensaries.filter(disp=>(disp.location === prop || disp.price === prop || disp.name === prop || disp.yearsOpen === prop ));
+        this.setState({filteredDispensaries: filteredDispensaries})
+    }
+    render() {
+        return (
+          <div className="puzzleBox filterObjectPB">
+            <h4> Filter Object </h4>
+            <span className="puzzleText"> Original: { JSON.stringify(this.state.dispensaries, null, 10) } </span>
+            <input className="inputLine" onChange={ e => this.handleChange(e.target.value) }/>
+            <button className="confirmationButton" onClick={  ()=> this.filterEmployees(this.state.userInput)}> Filter </button>
+            <span className="resultsBox filterObjectRB"> Filtered: { JSON.stringify(this.state.filteredEmployees, null, 10) } </span>
+          </div>
+        )
     }
 }
 
